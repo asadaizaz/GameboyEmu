f5b760e7e4421ffe6c39e9f5a01918e2caf56e5f
Asad Aizaz <asad.aizaz99@gmail.com>
2019-12-25 13:42:54 -0800

    Initial commit

---
+++ cpu/cpu.go
@@ -0,0 +1,19 @@
+package cpu
+
+type Registers struct {
+	A byte
+	B byte
+	C byte
+	D byte
+	E byte
+	H byte
+	L byte
+}
+
+type Cpu struct {
+	//Clock
+	// registers
+	// memory?
+	// Instructions
+
+}
